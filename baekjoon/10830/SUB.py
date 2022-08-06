7dbd85930c024dae4297efeab38eea6cd435c04a
jungsub123 <31717041+jungsub123@users.noreply.github.com>
2022-08-06 10:15:26 +0900

    [25주차] SUB-baekjoon-10830

--- baekjoon/10830/SUB.py
+++ baekjoon/10830/SUB.py
@@ -12,19 +12,28 @@ for num in range(1,37):
 
 cur = (1<<36)
 cur_num = 36
-print(cur)
-while(True):
-    if b > cur:
-        temp = [[0 for _ in range(n)] for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    temp[i][j] += (ans[i][k] * arr[cur_num][k][j]) % 1000
-        ans = temp.copy()
+first = 0
+
+while(cur_num >= 0):
+    if b >= cur:
+        if first == 0:
+            first = 1
+            ans = arr[cur_num].copy()
+        else:
+            temp = [[0 for _ in range(n)] for _ in range(n)]
+            for i in range(n):
+                for j in range(n):
+                    for k in range(n):
+                        temp[i][j] += ans[i][k] * arr[cur_num][k][j]
+                        temp[i][j]%=1000
+            ans = temp.copy()
+        b-=cur
 
     cur = cur>>1
     cur_num-=1
-    if cur_num < 0:
-        break
 
-print(ans)
+
+for i in range(n):
+    for j in range(n):
+        print(ans[i][j]%1000, end=' ')
+    print()
